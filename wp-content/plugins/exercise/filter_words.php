7767b5081cc909407ab28f2c83fbd0bf7e03dd61
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2024-06-24 22:20:43 +0430

    chore: Update plugin metadata and remove unused files

---
+++ wp-content/plugins/exercise/filter_words.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+Plugin Name: filter_word Anti-spam: Spam Protection
+Plugin URI: https://filter_word.com/
+Description: Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque labore minus autem amet? Doloribus a quae quaerat, dicta distinctio earum tempora magnam ducimus labore veritatis, illum quos delectus cumque voluptas.
+Version: 1.0.0
+Requires at least: 5.8
+Requires PHP: 5.6.20
+Author: Mahdi Rezaei - Weboxy Software solutions team.
+Author URI: https://MahdiSohaily.github.io/
+License: GPLv2 or later
+Text Domain: filter_word
+ */
